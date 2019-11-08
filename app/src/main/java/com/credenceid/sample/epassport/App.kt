dd93fb38cad5191147e64f92a1bb9dba4a853bdc
Avi Varshney <avarshney65@gmail.com>
2019-11-07 17:01:35 -0800

    Kotlin migration complete.

---
+++ app/src/main/java/com/credenceid/sample/epassport/App.kt
@@ -0,0 +1,21 @@
+package com.credenceid.sample.epassport
+
+import android.annotation.SuppressLint
+import android.app.Application
+
+import com.credenceid.biometrics.BiometricsManager
+import com.credenceid.biometrics.DeviceFamily
+import com.credenceid.biometrics.DeviceType
+
+@SuppressLint("StaticFieldLeak")
+class App : Application() {
+
+    companion object {
+        /* CredenceSDK biometrics object used to interface with APIs. */
+        lateinit var BioManager: BiometricsManager
+        /* Stores which Credence family of device's this app is running on. */
+        var DevFamily = DeviceFamily.InvalidDevice
+        /* Stores which specific device this app is running on. */
+        var DevType = DeviceType.InvalidDevice
+    }
+}
---
+++ app/src/main/java/com/credenceid/sample/epassport/LaunchActivity.kt
@@ -0,0 +1,50 @@
+package com.credenceid.sample.epassport
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.LENGTH_SHORT
+import com.credenceid.biometrics.Biometrics
+import com.credenceid.biometrics.Biometrics.ResultCode.*
+import com.credenceid.biometrics.BiometricsManager
+
+class LaunchActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        this.initBiometrics()
+    }
+
+    private fun initBiometrics() {
+
+        /*  Create new biometrics object. */
+        App.BioManager = BiometricsManager(this)
+
+        /* Initialize object, meaning tell CredenceService to bind to this application. */
+        App.BioManager.initializeBiometrics { resultCode: Biometrics.ResultCode, _: String, _: String ->
+
+            when (resultCode) {
+                OK -> {
+                    Toast.makeText(this, getString(R.string.bio_init_done), LENGTH_SHORT).show()
+
+                    App.DevFamily = App.BioManager.deviceFamily
+                    App.DevType = App.BioManager.deviceType
+
+                    /* Launch main activity. */
+                    val intent = Intent(this, MRZActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    this.finish()
+
+                }
+                INTERMEDIATE -> {
+                    /* This code is never returned for this API. */
+                }
+                FAIL -> Toast.makeText(this, getString(R.string.bio_ini_fail), LENGTH_LONG).show()
+            }
+        }
+    }
+}
